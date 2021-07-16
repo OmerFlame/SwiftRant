2acc9c3781adb98ed2eaea1aa26f2ddc85530f15
Omer Shamai <renginald.bagel@gmail.com>
2021-07-16 10:15:42 +0300

    Created project scaffold

---
+++ Tests/SwiftRantTests/SwiftRantTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftRant
+
+final class SwiftRantTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftRant().text, "Hello, World!")
+    }
+}
